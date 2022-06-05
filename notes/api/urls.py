7c10ccb460f10809551b32b83b02f37f6ac2d6ac
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-06-05 22:19:43 +0530

    * Added GET Notes * Added GET single note

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('notes/', views.getNotes, name="notes"),
+    path('notes/<str:pk>', views.getNote, name="note"),
 ]
