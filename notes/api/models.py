6ea72f96966b2fc952a96334f528dd89d2662c28
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-06-05 21:53:46 +0530

    Added Notes Model

--- notes/api/models.py
+++ notes/api/models.py
@@ -1,3 +1,12 @@
+from turtle import update
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.body[0:50]
--- notes/api/views.py
+++ notes/api/views.py
@@ -4,4 +4,36 @@ from django.http import JsonResponse
 # Create your views here.
 
 def getRoutes(request):
-    return JsonResponse("Our API", safe=False)
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'   
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Create new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'  
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes existing notes'  
+        },
+    ]
+    return JsonResponse(routes, safe=False)
