827a2d822e4d708e9535034b88caec4fe595a32c
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-06-06 21:54:13 +0530

    Fixed serializer response error

--- notes/api/utils.py
+++ notes/api/utils.py
@@ -32,7 +32,7 @@ def updateNote(request, pk):
     if serializer.is_valid():
         serializer.save()
 
-    return serializer.data
+    return Response(serializer.data)
 
 
 def deleteNote(request, pk):
